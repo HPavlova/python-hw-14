08e0ded55c17ac7af8d30de9d36839c49a5e4af1
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-11-15 20:13:16 +0200

    add page parser and writing data to json

--- main.py
+++ main.py
@@ -2,25 +2,33 @@ from bs4 import BeautifulSoup
 import json
 import requests
 
-url = 'https://quotes.toscrape.com/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-quotes = soup.findAll('div', class_="quote")
-data = []
 
-for quote in quotes:
-    result = {}
+def main():
+    url = 'https://quotes.toscrape.com/page/{}/'
+    data = []
 
-    result['text'] = quote.find('span', class_="text").text
-    result['author'] = quote.find('small', class_="author").text
-    result['author-link'] = quote.find('a').get('href')
+    for page in range(1, 3):
+        response = requests.get(url.format(page))
+        soup = BeautifulSoup(response.text, 'html.parser')
+        quotes = soup.findAll('div', class_="quote")
 
-    tags = quote.find('div', class_="tags").findAll('a', class_='tag')
-    tags_text = []
-    for tag in tags:
-        text = tag.text
-        tags_text.append(text)
-    result['tags'] = tags_text
-    data.append(result)
+        for quote in quotes:
+            result = {}
 
-print(data)
+            result['text'] = quote.find('span', class_="text").text.replace('“', '').replace('”', '')
+            result['author'] = quote.find('small', class_="author").text
+            result['author-link'] = quote.find('a').get('href')
+
+            tags = quote.find('div', class_="tags").findAll('a', class_='tag')
+            tags_text = []
+            for tag in tags:
+                text = tag.text
+                tags_text.append(text)
+            result['tags'] = tags_text
+            data.append(result)
+    with open('quotes.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    main()
