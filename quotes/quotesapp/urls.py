47fdb4344c60583303f223bd6980d601d5c724d2
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-11-22 22:30:32 +0200

    delete Django project

--- quotes/quotesapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('filter_by_author', views.filter_by_author, name='filter_by_author'),
-]
