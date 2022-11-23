08363749b492b8b93fedbacf2f7967b07c83ba20
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-11-23 21:52:56 +0200

    Update authors.py

--- spyder/spyder/spiders/authors.py
+++ spyder/spyder/spiders/authors.py
@@ -1,4 +1,7 @@
 import scrapy
+from scrapy.spiders import CrawlSpider
+import xpath
+from spyder.spyder.items import SpyderItem
 
 
 class AuthorsSpider(scrapy.Spider):
@@ -7,4 +10,13 @@ class AuthorsSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        for quote in response.xpath("/html//div[@class='quote']"):
+            yield ({
+                'text': quote.xpath("span[@class='text']/text()").get(),
+                'author': quote.xpath("span/small/text()").extract(),
+                'author-link': quote.xpath("a/@href").get(),
+                'tags': quote.xpath("div[@class='tags']/a/text()").extract()
+            })
+        next_link = response.xpath("//li[@class='next']/a/@href").get()
+        if next_link:
+            yield scrapy.Request(url=self.start_urls[0]+next_link)
